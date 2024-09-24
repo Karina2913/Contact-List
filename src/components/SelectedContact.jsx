76952731ae03dc6741277b70d4fd7992abb21a05
Karina <karinapliegovergara@gmail.com>
2024-09-24 18:11:17 -0500

    feat: updated fetch

---
+++ src/components/SelectedContact.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
+
+export default function SelectedContact ({ selectedContactId, setSelectedContactId }) {
+    const [contact, setContact] = useState(null);
+
+    useEffect(() => {
+        async function fetchContacts() {
+            try {
+                const response = await fetch(`https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users/${selectedContactId}`);
+                const result = await response.json();
+                setContact(result);
+                console.log(result);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchContacts();
+    }, []);
+}
